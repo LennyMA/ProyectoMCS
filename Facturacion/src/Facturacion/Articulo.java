657f8e9f1a1b903435c202036f61838de2f20109
YadiraAllauca <yallauca3770@uta.edu.ec>
2022-01-18 11:11:42 -0500

    Se generaron 3 tipos de constructores en la clase Articulo

--- Facturacion/src/Facturacion/Articulo.java
+++ Facturacion/src/Facturacion/Articulo.java
@@ -5,4 +5,20 @@ public class Articulo {
     String codigo, nombreArticulo;
     int cantidad;
     float precio;
+
+    public Articulo() {
+    }
+
+    public Articulo(String codigo, String nombreArticulo, int cantidad, float precio) {
+        this.codigo = codigo;
+        this.nombreArticulo = nombreArticulo;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public Articulo(String nombreArticulo, int cantidad) {
+        this.nombreArticulo = nombreArticulo;
+        this.cantidad = cantidad;
+    }
+
 }
