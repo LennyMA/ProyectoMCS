029079e4e06cb3ad5aac7e89bb2bcd1d51869572
Lenin <lmoreno3918@uta.edu.ec>
2022-01-19 19:35:51 -0500

    Se agregaron los atributos de la clase Cliente y 2 metodos constructores

--- Facturacion/src/Facturacion/Cliente.java
+++ Facturacion/src/Facturacion/Cliente.java
@@ -1,5 +1,24 @@
 package Facturacion;
 
+import java.util.LinkedList;
+
 public class Cliente {
-    
+
+    String nombre, apellido, direccion, id, telefono;
+    LinkedList<Compra> compras;
+    LinkedList<Compra> facturas;
+
+    public Cliente() {
+
+    }
+
+    public Cliente(String nombre, String apellido, String id, String direccion, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.id = id;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.compras = new LinkedList();
+        this.facturas = new LinkedList();
+    }
 }
