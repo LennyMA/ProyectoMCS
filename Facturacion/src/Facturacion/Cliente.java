a819469005ff4cf0d1e55d4a346f5888b8b6dfdf
Lenin <lmoreno3918@uta.edu.ec>
2022-01-24 22:29:08 -0500

    Se agregaron los metodos: buscarCliente, agregarCliente, getNombre, getDireccion, toString en la clase Cliente

--- Facturacion/src/Facturacion/Cliente.java
+++ Facturacion/src/Facturacion/Cliente.java
@@ -21,4 +21,50 @@ public class Cliente {
         this.compras = new LinkedList();
         this.facturas = new LinkedList();
     }
+
+    public int buscarCliente(LinkedList<Cliente> cliente, String id) {
+        for (int i = 0; i < cliente.size(); i++) {
+            if (cliente.get(i).id.equals(id)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean agregarCliente(LinkedList<Cliente> cliente, String nombreCliente,
+            String apellidoCliente, String idCliente, String direccionCliente, String telefonoCliente) {
+        int pos = buscarCliente(cliente, idCliente);
+        if (pos != -1) {
+            return false;
+        } else {
+            cliente.add(new Cliente(nombreCliente, apellidoCliente, idCliente, direccionCliente, telefonoCliente));
+            return true;
+        }
+    }
+    
+    //un metodo de promedio compras?
+
+    public String getNombre() {
+        if (this.nombre.length() > 7) {
+            return this.nombre.substring(0, 7);
+        } else if (this.nombre.length() < 6) {
+            return this.nombre + "  ";
+        }
+        return this.nombre;
+    }
+    
+    public String getDireccion(){
+        if(this.direccion.length() > 13){
+            return this.direccion.substring(0, 13);
+        }else if(this.direccion.length() < 10){
+            return this.direccion + "   ";
+        }
+        return this.direccion;
+    }
+    
+    @Override
+    public String toString(){
+        return getNombre() + " " + this.apellido + " " + this.id + " "
+                + this.getDireccion() + " " + this.telefono;
+    }
 }
