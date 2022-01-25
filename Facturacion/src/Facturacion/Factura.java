a3db25e94c81828fc7caafbee232ae5384b7d77b
Lenin <lmoreno3918@uta.edu.ec>
2022-01-25 00:20:37 -0500

    Revert "Se agregaron los atributos a la clase Factura, 2 metodos Constructores y un metodo buscarFactura"
    This reverts commit a3e690c0e00e75a304168fae1d5612299efd378f.

--- Facturacion/src/Facturacion/Factura.java
+++ Facturacion/src/Facturacion/Factura.java
@@ -1,41 +1,5 @@
 package Facturacion;
 
-import java.util.LinkedList;
-
 public class Factura {
-    
-    LinkedList<Compra> compras;
-    Cliente cliente;
-    Dependiente dependiente;
-    boolean cancelado;
-    int codigo;
-    final float IVA = 0.12f;
-    
-    
-    public Factura(){
-        
-    }
-    
-    public Factura(Cliente cliente, LinkedList<Compra> compras, Dependiente dependiente,
-            int codigo, boolean cancelado){
-        this.cliente = cliente;
-        this.compras = compras;
-        this.dependiente = dependiente;
-        this.codigo = codigo;
-        this.cancelado = cancelado;
-        
-    }
-    
-    public Factura buscarFactura(LinkedList<Factura> facturas, int codigo){
-        if(!facturas.isEmpty()){
-            for(int i = 0; i < facturas.size(); i++){
-                if(facturas.get(i).codigo == codigo){
-                    return facturas.get(i);
-                }
-            }
-        }
-        System.out.println("\nNo existen facturas");
-        return null;
-    }
     
 }
