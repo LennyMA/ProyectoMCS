3c1e6db55122724d3b30d2e9ea92387468b313e0
Melany <recaldealisson05@gmail.com>
2022-01-26 00:13:35 -0500

    Implementacion metodo subTotal en la clase Compra

--- Facturacion/src/Facturacion/Compra.java
+++ Facturacion/src/Facturacion/Compra.java
@@ -5,11 +5,15 @@ public class Compra {
     float subTotal, precioUnitario;
     int cantidadProducto;
     Articulo articulo;
-    
-     public Compra(int cantidadProducto, Articulo nombreArticulo) {
+
+    public Compra(int cantidadProducto, Articulo nombreArticulo) {
         this.cantidadProducto = cantidadProducto;
         this.articulo = nombreArticulo;
 
     }
-     
+
+    public float subTotal() {
+        return this.subTotal = this.cantidadProducto * this.articulo.precio;
+    }
+
 }
